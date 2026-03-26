62b38bbc58918131b899770dbb2e2efc1df63908
Jimenez-1503 <pedro.jimenez@aluno.senai.br>
2026-03-26 10:34:33 -0300

    get,post,put e delete

--- api-sabordigital/src/app.js
+++ api-sabordigital/src/app.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 
 app.get('/produtos', async (req,res) => {
     try{
-        const produtos = await queryAsync('SELECT * FROM produto ORDER BY id')
+        const produtos = await queryAsync('SELECT * FROM produto ORDER BY id DESC')
 
         res.json({
             sucesso: true,
@@ -78,12 +78,12 @@ app.get('/produtos/:id', async (req, res) => {
 
 app.post('/produtos', async(req,res) =>{
     try {
-        const {id, nome, descricao, preco, disponibilidade } = req.body
+        const {nome, descricao, preco, disponivel } = req.body
 
-        if(!nome || !descricao || !preco || !disponibilidade ){
+        if(!nome || !descricao || preco == undefined ){
             return res.status(400).json({
                 sucesso: false,
-                mensagem: 'Nome, descrição, preço e disponibilidade são obrigatórios'
+                mensagem: 'Nome, descrição e preço  são obrigatórios'
             })
         }
 
@@ -97,8 +97,8 @@ app.post('/produtos', async(req,res) =>{
         const novoProduto = {
             nome: nome.trim(),
             descricao: descricao.trim(),
-            duracao,
-            disponibilidade: disponibilidade.trim()
+            preco,
+            disponivel: disponivel || true 
         }
 
         const resultado = await queryAsync('INSERT INTO produto SET ?',[novoProduto])
@@ -118,4 +118,107 @@ app.post('/produtos', async(req,res) =>{
     }
 } )
 
+app.put('/produtos/:id', async (req, res) => {
+    try {
+        
+        const { id } = req.params
+        const {nome, descricao, preco, disponivel } = req.body
+
+        if (!id || isNaN(id)) {
+            return res.status(400).json({
+                sucesso: false,
+                mensagem: 'ID do produto inválido.'
+            })
+        }
+
+        const produtoExiste = await queryAsync('SELECT * FROM produto WHERE id = ?', [id])
+
+        if (produtoExiste.length === 0) {
+            return res.status(404).json({
+                sucesso: false,
+                mensagem: 'Produto não encontrado.'
+            })
+        }
+
+        const produtoAtualizado = {}
+
+        if (nome !== undefined) produtoAtualizado.nome = nome.trim()
+        if (descricao !== undefined) produtoAtualizado.descricao = descricao.trim()
+
+        if (preco !== undefined) {
+            if (typeof preco !== 'number' || preco <= 0) {
+                return res.status(400).json({
+                    sucesso: false,
+                    mensagem: 'Preço deve ser um número positivo.'
+                })
+
+            } produtoAtualizado.preco = preco }
+
+        if (disponivel !== undefined) produtoAtualizado.disponivel = disponivel
+
+        if (Object.keys(produtoAtualizado).length === 0) {
+            return res.status(400).json({
+                sucesso: false,
+                mensagem: 'Nenhum campo para atualizar.'
+            })
+        }
+
+        await queryAsync('UPDATE produto SET ? WHERE id = ?', [produtoAtualizado, id])
+
+        res.json({
+            sucesso: true,
+            mensagem: 'Produto atualizado com sucesso.'
+        })
+
+    } catch (erro) {
+
+        console.error('Erro ao atualizar produto:', erro)
+        res.status(500).json({
+            sucesso: false,
+            mensagem: 'Erro ao atualizar produto.',
+            erro: erro.message
+        })
+
+    }
+})
+
+app.delete('/produtos/:id', async (req, res) => {
+    try {
+
+        const {id} = req.params
+
+       
+        if (!id || isNaN(id)) {
+            return res.status(400).json({
+                sucesso: false,
+                mensagem: 'ID do produto inválido.'
+            })
+        }
+
+        const filmeExiste = await queryAsync('SELECT * FROM produto WHERE id = ?', [id])
+
+        if (filmeExiste.length === 0) {
+            return res.status(404).json({
+                sucesso: false,
+                mensagem: 'Produto não encontrado.'
+            })
+        }
+
+        await queryAsync('DELETE FROM produto WHERE id = ?', [id])
+
+        res.status(200).json({
+            sucesso: true,
+            mensagem: 'Produto deletado com sucesso.'
+        })
+
+    } catch (erro) {
+        console.error('Erro ao deletar produto:', erro)
+        res.status(500).json({
+            sucesso: false,
+            mensagem: 'Erro ao deletar produto.',
+            erro: erro.message
+        })
+    }
+
+})
 module.exports = app
